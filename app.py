f02e889e8793aa782257448c6e00b1e62159faf6
akkharawinch68-lgtm <akkharawin.ch.68@ubu.ac.th>
2026-02-26 02:28:20 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
